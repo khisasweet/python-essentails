089f4cf38146beb1e3b469f77b73e5e03c8918c2
khisasweet <105873391+khisasweet@users.noreply.github.com>
2024-11-12 14:01:02 -0500

    Update private-property.py

--- private-property.py
+++ private-property.py
@@ -7,3 +7,13 @@ class Employee:
 Steve = Employee(3789, 2500)
 print("ID:", Steve.ID)
 print("Salary:", Steve.__salary)  # this will cause an error
+
+#### example showing how to access private property
+class Employee:
+    def __init__(self, ID, salary):
+        self.ID = ID
+        self.__salary = salary  # salary is a private property
+
+
+Steve = Employee(3789, 2500)
+print(Steve._Employee__salary)  # accessing a private property
