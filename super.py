8c7460d4d3b6b63b1bf4d68cc213256b7e606eba
khisasweet <105873391+khisasweet@users.noreply.github.com>
2024-11-13 11:48:57 -0500

    Create super.py

---
+++ super.py
@@ -0,0 +1,78 @@
+class Vehicle:  # defining the parent class
+    fuelCap = 90
+
+
+class Car(Vehicle):  # defining the child class
+    fuelCap = 50
+
+    def display(self):
+        # accessing fuelCap from the Vehicle class using super()
+        print("Fuel cap from the Vehicle Class:", super().fuelCap)
+
+        # accessing fuelCap from the Car class using self
+        print("Fuel cap from the Car Class:", self.fuelCap)
+
+
+obj1 = Car()  # creating a car object
+obj1.display()  # calling the Car class method display()
+
+###############
+
+class Vehicle:  # defining the parent class
+    def display(self):  # defining display method in the parent class
+        print("I am from the Vehicle Class")
+
+
+class Car(Vehicle):  # defining the child class
+    # defining display method in the child class
+    def display(self):
+        super().display()
+        print("I am from the Car Class")
+
+
+obj1 = Car()  # creating a car object
+obj1.display()  # calling the Car class method display()
+
+
+############
+class ParentClass():
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+
+class ChildClass(ParentClass):
+    def __init__(self, a, b, c):
+        super().__init__(a, b)
+        self.c = c
+
+
+obj = ChildClass(1, 2, 3)
+print(obj.a)
+print(obj.b)
+print(obj.c)
+##########
+class Vehicle:
+    def __init__(self, make, color, model):
+        self.make = make
+        self.color = color
+        self.model = model
+
+    def printDetails(self):
+        print("Manufacturer:", self.make)
+        print("Color:", self.color)
+        print("Model:", self.model)
+
+
+class Car(Vehicle):
+    def __init__(self, make, color, model, doors):
+        Vehicle.__init__(self, make, color, model)
+        self.doors = doors
+
+    def printCarDetails(self):
+        self.printDetails()
+        print("Door:", self.doors)
+
+
+obj1 = Car("Suzuki", "Grey", "2015", 4)
+obj1.printCarDetails()
