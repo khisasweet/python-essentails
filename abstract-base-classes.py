3a079dac17f1b33a5277d10704a194d9ebaa96c4
khisasweet <105873391+khisasweet@users.noreply.github.com>
2024-11-13 14:16:29 -0500

    Update abstract-base-classes.py

--- abstract-base-classes.py
+++ abstract-base-classes.py
@@ -19,3 +19,31 @@ class Square(Shape):
 shape = Shape()
 # this code will not compile since Shape has abstract methods without
 # method definitions in it
+############################
+from abc import ABC, abstractmethod
+
+
+class Shape(ABC):  # Shape is a child class of ABC
+    @abstractmethod
+    def area(self):
+        pass
+
+    @abstractmethod
+    def perimeter(self):
+        pass
+
+
+class Square(Shape):
+    def __init__(self, length):
+        self.length = length
+
+    def area(self):
+        return (self.length * self.length)
+
+    def perimeter(self):
+        return (4 * self.length)
+
+
+shape = Shape()
+# this code will not compile since Shape has abstract methods without
+# method definitions in it
